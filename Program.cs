1889650f9d421c6edbc3979138fc89535e7285a3
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-30 14:45:17 -0500

    First Commit:

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DoYouLikePizza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            bool pizzaOrNot;
+            string pizzaResponse;
+                        
+            Console.WriteLine("What is your name? ");
+            userName = Console.ReadLine();
+
+            Console.WriteLine("Do you like pizza? ");
+            pizzaResponse = Console.ReadLine();
+
+            if (pizzaResponse == "yes")
+            {
+                pizzaOrNot = true;
+                Console.WriteLine(userName + " likes pizza!");
+            }
+            else
+            {
+                Console.WriteLine(userName + " does not like pizza!");
+            }
+        }
+    }
+}
